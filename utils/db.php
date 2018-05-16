5b17657df5cc01ecb974f682cac7fc30bbfc7f01
tounssy <adnane.ben-ali@live.fr>
2018-05-16 14:38:36 +0200

    ajout css

--- utils/db.php
+++ utils/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=meme-generator;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=meme-generator;charset=utf8', 'adnane', 'piccolo333');
 }
 catch(Exception $e) {
     die('Erreur:'.$e->getMessage());
