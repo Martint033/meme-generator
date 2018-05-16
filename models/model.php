91e6d0e2a89b2a85d5f00253a4b04302bf1935ff
Martint033 <martint033@gmail.com>
2018-05-16 09:51:26 +0200

    upload file

--- models/model.php
+++ models/model.php
@@ -1,4 +1,17 @@
 <?php 
 // ce model contient toutes les requêtes 
 
+require_once("utils/db.php")
 
+
+function listImg () {
+    $response = $GLOBALS['bdd']->prepare('SELECT * FROM image');
+    $response->execute();
+    return $response->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+function addImg ($img, $title) {
+    $response = $GLOBALS['bdd']->prepare('INSERT INTO `image`(`title`, `image`) VALUES ('.$title.' , '.$img.')');
+    $response->execute();
+}
