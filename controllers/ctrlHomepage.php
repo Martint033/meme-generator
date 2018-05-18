1f10dcfbe3ed2c407f45d4d91037fd68257324ca
tounssy <adnane.ben-ali@live.fr>
2018-05-18 11:41:08 +0200

    fix bdd

--- controllers/ctrlHomepage.php
+++ controllers/ctrlHomepage.php
@@ -3,7 +3,7 @@
 
     $posts = listImg();
 
-    require_once 'vendor/autoload.php';
+    require_once ('vendor/autoload.php');
 
     $loader = new Twig_Loader_Filesystem('views');
     $twig = new Twig_Environment($loader, array(
