ebc63c81ba635e3c62c536c4fa668907304ac346
Z-dorf <sebastian.zanon2@gmail.com>
2018-05-22 15:29:39 +0200

    update

--- controllers/ctrlUpload-meme.php
+++ controllers/ctrlUpload-meme.php
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-<?php 
-require("models/model.php");
-echo'coucou';
-$title = $_POST['title'];
-$url = $target_dir.'/'.$target_file;
-$idImg = addImg($title,$nom, $url);
-
-
-require_once 'vendor/autoload.php';
-
-
-$upload_dir = upload_meme();  //implement this function yourself
-$img = $_POST['download'];
-// $img = str_replace('data:image/png;base64,', '', $img);
-// $img = str_replace(' ', '+', $img);
-$data = base64_decode($img);
-$file = $upload_dir."image_name.png";
-$success = file_put_contents($file, $data);
-
-
-// $template = $twig->load('upload-meme.html');
-
-header('Location: /meme-generator/upload-meme/'.$nom."&".$idImg);
-?>
-=======
 <?php
 $data = $_POST['save'];
 
@@ -47,4 +21,3 @@ addMeme($title, $memeURL, $newName_m, $id_picture);
 
   
 //header('Location: /meme-generator/edit-meme/'.$nom."&".$idImg); 
->>>>>>> 715a16f3460849bfa2fa8ecbbaf58a269813cf23
